cd20c45c28cf9f9d45641801e077bc46900cd1b6
=Hoangg Nhatt <nhathx18@gmail.com>
2024-08-12 23:51:13 +0700

    bai10

---
+++ BT/Chapter2/Part1/bai10.py
@@ -0,0 +1,16 @@
+def main():
+   a = float(input('Enter the length of the room: '))
+   b = float(input('Enter the width of the room: '))
+
+   if a < b :
+        print("Warning: The length of the room is less than the width.") 
+   else : 
+        S = a * b
+        print(f'The area of the room is {S:.2f}')
+
+
+
+
+
+if __name__ == '__main__':
+    main()
