0954d897fa7765ab65815b7bc577b171fac5e2f5
=Hoangg Nhatt <nhathx18@gmail.com>
2024-08-13 00:11:23 +0700

    bai 6

--- BT/Chapter2/Part1/bai6.py
+++ BT/Chapter2/Part1/bai6.py
@@ -4,7 +4,7 @@ def main():
     r = float(input('Enter the radius of the cyclinder : '))
     h = float(input('Enter the height of the cyclinder: '))
 
-    V = math.pi * r*r*h
+    V = math.pi * r * r * h
     print(f'Volume of the cyclinder is : {V:.2f}')
 
 
