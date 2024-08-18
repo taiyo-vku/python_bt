34e0988da924386a77b3369502a8d2e39e0153e7
=Hoangg Nhatt <nhathx18@gmail.com>
2024-08-18 10:22:58 +0700

    bai 14

---
+++ BT/Chapter2/Part1/bai14.py
@@ -0,0 +1,20 @@
+def main():
+    a = int(input('Enter the value of a : '))
+    b = int(input('Enter the value of b : '))
+
+    s = a + b
+    diff = a - b
+    pro = a * b
+    quotient = a / b
+
+
+    print(f"The sum of {a} and {b} is: {s:.2f}")
+    print(f"The difference between {a} and {b} is: {diff:.2f}")
+    print(f"The product of {a} and {num2} is: {pro:.2f}")
+    print(f'The quotient of {a} divided by {b} is: {quotient:.2f}')
+
+
+
+
+if __name__ == '__main__':
+    main()
